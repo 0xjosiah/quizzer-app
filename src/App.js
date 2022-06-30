187ac18b6273601d8ec6b00cd89fdfbc3294cba5
Josiah Webb <0xjosiah@MacBook-Pro.lan>
2022-06-29 21:02:17 -0600

    gathers answers in array, adds helper shuffle fn, shuffles answers and renders them

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [quiz, setQuiz] = useState({})
 
   const fetchQs = async () => {
-    const res = await fetch('https://opentdb.com/api.php?amount=5')
+    const res = await fetch('https://opentdb.com/api.php?amount=5&type=multiple')
     const data = await res.json()
     console.log(data)
     setQuiz(data)
--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,22 @@
+import { shuffle } from "../helpers"
+
 export default function Quiz(props) {
-    const questions = props.quiz.results.map(q => (
-        <div>
-            <h4 dangerouslySetInnerHTML={{__html:`${q.question}`}}></h4>
-        </div>
-    ))
+
+    const questions = props.quiz.results.map(q => {
+        const answers = q.incorrect_answers.concat(q.correct_answer)
+        const shuffledAs = shuffle(answers)
+        const answersHtml = shuffledAs.map(i => (
+            <button dangerouslySetInnerHTML={{__html:`${i}`}}></button>
+        ))
+        
+        return (
+            <div>
+                <h4 dangerouslySetInnerHTML={{__html:`${q.question}`}}></h4>
+                {answersHtml}
+            </div>
+        )
+    })
+
     return (
         <div>
             {questions}
