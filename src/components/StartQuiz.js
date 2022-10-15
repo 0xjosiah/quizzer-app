978fd9c52ee7fbdd0b7cb2f389ad317687590ea6
Josiah Webb <jj.webb520@gmail.com>
2022-10-14 18:45:33 -0600

    creates option select for categories (not functional)

--- src/components/StartQuiz.js
+++ src/components/StartQuiz.js
@@ -7,6 +7,33 @@ export default function StartQuiz(props) {
             <div className="title-card">
                 <h1 className="title-card__header">Quizzical</h1>
                 <p className="title-card__description">Test your skills with the ultimate quiz!</p>
+                <label for="question-category">Select a category</label>
+                <select name="question-category" id="question-category">
+                    <option id="9">General Knowledge</option> 
+                    <option id="10">Entertainment: Books</option> 
+                    <option id="11">Entertainment: Film</option> 
+                    <option id="12">Entertainment: Music</option> 
+                    <option id="13">Entertainment: Musicals & Theatres</option> 
+                    <option id="14">Entertainment: Television</option> 
+                    <option id="15">Entertainment: Video Games</option> 
+                    <option id="16">Entertainment: Board Games</option> 
+                    <option id="17">Science & Nature</option> 
+                    <option id="18">Science: Computers</option> 
+                    <option id="19">Science: Mathematics</option> 
+                    <option id="20">Mythology</option> 
+                    <option id="21">Sports</option> 
+                    <option id="22">Geography</option> 
+                    <option id="23">History</option> 
+                    <option id="24">Politics</option> 
+                    <option id="25">Art</option> 
+                    <option id="26">Celebrities</option> 
+                    <option id="27">Animals</option> 
+                    <option id="28">Vehicles</option> 
+                    <option id="29">Entertainment: Comics</option> 
+                    <option id="30">Science: Gadgets</option> 
+                    <option id="31">Entertainment: Japanese Anime & Manga</option> 
+                    <option id="32">Entertainment: Cartoon & Animations</option> 
+                </select>
                 <button 
                     className="title-card__start-btn"
                     onClick={props.fetchQs}
